a18f12a0aef034499151cb810573a72916b86f1c
k.gotou <k.gotou@azoom.jp>
2018-09-13 14:18:47 +0900

    fix(snackbar): rscss

--- packages/mdc-snackbar/constants.js
+++ packages/mdc-snackbar/constants.js
@@ -22,18 +22,18 @@
  */
 export const cssClasses = {
   ROOT: 'mdc-snackbar',
-  TEXT: 'mdc-snackbar__text',
-  ACTION_WRAPPER: 'mdc-snackbar__action-wrapper',
-  ACTION_BUTTON: 'mdc-snackbar__action-button',
-  ACTIVE: 'mdc-snackbar--active',
-  MULTILINE: 'mdc-snackbar--multiline',
-  ACTION_ON_BOTTOM: 'mdc-snackbar--action-on-bottom',
+  TEXT: 'text',
+  ACTION_WRAPPER: 'actionwrapper',
+  ACTION_BUTTON: 'actionbutton',
+  ACTIVE: '-active',
+  MULTILINE: '-multiline',
+  ACTION_ON_BOTTOM: '-actionbottom',
 };
 
 export const strings = {
-  TEXT_SELECTOR: '.mdc-snackbar__text',
-  ACTION_WRAPPER_SELECTOR: '.mdc-snackbar__action-wrapper',
-  ACTION_BUTTON_SELECTOR: '.mdc-snackbar__action-button',
+  TEXT_SELECTOR: '.text',
+  ACTION_WRAPPER_SELECTOR: '.actionwrapper',
+  ACTION_BUTTON_SELECTOR: '.actionbutton',
   SHOW_EVENT: 'MDCSnackbar:show',
   HIDE_EVENT: 'MDCSnackbar:hide',
 };
